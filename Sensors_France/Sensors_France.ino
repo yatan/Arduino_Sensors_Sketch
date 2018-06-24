9b6d65fd14da25423a861c71d7100540f9996540
Fran <yatan87@gmail.com>
2018-06-24 14:06:56 +0200

    ASCII ART

--- Sensors_France/Sensors_France.ino
+++ Sensors_France/Sensors_France.ino
@@ -37,7 +37,15 @@
 const boolean debug = true;
 
 
-// Define Number of Sensors
+/*
+  _   _ _    _ __  __ ____  ______ _____     _____ ______ _   _  _____  ____  _____   _____ 
+ | \ | | |  | |  \/  |  _ \|  ____|  __ \   / ____|  ____| \ | |/ ____|/ __ \|  __ \ / ____|
+ |  \| | |  | | \  / | |_) | |__  | |__) | | (___ | |__  |  \| | (___ | |  | | |__) | (___  
+ | . ` | |  | | |\/| |  _ <|  __| |  _  /   \___ \|  __| | . ` |\___ \| |  | |  _  / \___ \ 
+ | |\  | |__| | |  | | |_) | |____| | \ \   ____) | |____| |\  |____) | |__| | | \ \ ____) |
+ |_| \_|\____/|_|  |_|____/|______|_|  \_\ |_____/|______|_| \_|_____/ \____/|_|  \_\_____/ 
+ */
+
 const int num_T = 4;   // Temperature of the culture. Sensor DS18B20.MAX 6
 						// T1_s T1_b
 const int num_DHT = 1; //Humidity and temperature ambient sensor. MAX 3
@@ -52,7 +60,14 @@ const boolean option_SD = true;   //if SD connexion posible (=1) or not (=0)
 const boolean option_clock = true; //if clock posible (=1) or not (=0)
 
 
-// Pins
+/*
+  _____ _____ _   _  _____ 
+ |  __ \_   _| \ | |/ ____|
+ | |__) || | |  \| | (___  
+ |  ___/ | | | . ` |\___ \ 
+ | |    _| |_| |\  |____) |
+ |_|   |_____|_| \_|_____/                            
+*/
 #define pin_onewire 0    // where 1-wire is connected
 // Pin lector SD
 #define pin_sd_card 4
@@ -63,10 +78,20 @@ const int pins_pir[num_PIR] = {20};
 
 
 
+/*
+   _____ _      ____  ____          _       __      __     _____   _____ 
+  / ____| |    / __ \|  _ \   /\   | |      \ \    / /\   |  __ \ / ____|
+ | |  __| |   | |  | | |_) | /  \  | |       \ \  / /  \  | |__) | (___  
+ | | |_ | |   | |  | |  _ < / /\ \ | |        \ \/ / /\ \ |  _  / \___ \ 
+ | |__| | |___| |__| | |_) / ____ \| |____     \  / ____ \| | \ \ ____) |
+  \_____|______\____/|____/_/    \_\______|     \/_/    \_\_|  \_\_____/ 
+*/
 
-// Global sensors variables
 OneWire oneWireObjeto(pin_onewire);
 DallasTemperature sensorDS18B20(&oneWireObjeto);
+// Array de temperatures amb tamany num_temp sensors assignats
+int array_temps[num_T];
+// LCD I2C
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 // RTC DS3231 (clock sensor)
 RTC_DS3231 rtc;
@@ -126,23 +151,43 @@ void mostra_LCD() {
 
 }
 
+/*
+  _      ____   ____  _____  
+ | |    / __ \ / __ \|  __ \ 
+ | |   | |  | | |  | | |__) |
+ | |   | |  | | |  | |  ___/ 
+ | |___| |__| | |__| | |     
+ |______\____/ \____/|_|     
+ */
+
 void loop() {
 
-  // Definim un array de temperatures amb tamany num_temp sensors assignats, aquesta variable pot ser global
-  int array_temps[num_T];
-  // Cridem la funció per a la recepció de temperatures enviant la direcció de memòria de la variable del array
-  capture_temps(array_temps);
-
-  // Exemple sortida dades
-  for(int i = 0; i < num_T; i++)
-  {
-    Serial.print("Sensor: ");
-    Serial.print(i);
-    Serial.print(" ");
-    Serial.println(array_temps[i]);
+  // Si tenim sondes de temperatura
+  if(num_T > 0) {
+    // Cridem la funció per a la recepció de temperatures enviant la direcció de memòria de la variable del array
+    capture_temps(array_temps);
+    // Exemple sortida dades
+    for(int i = 0; i < num_T; i++)
+    {
+      Serial.print("Sensor: ");
+      Serial.print(i);
+      Serial.print(" ");
+      Serial.println(array_temps[i]);
+    }    
   }
+
+  // END Loop()
   delay(2000);
 }
+
+/*
+   _____ ______ _______ _    _ _____  
+  / ____|  ____|__   __| |  | |  __ \ 
+ | (___ | |__     | |  | |  | | |__) |
+  \___ \|  __|    | |  | |  | |  ___/ 
+  ____) | |____   | |  | |__| | |     
+ |_____/|______|  |_|   \____/|_|                                                                            
+*/
 
 void setup() {
   // start serial port:
