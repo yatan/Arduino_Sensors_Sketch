a59cb327c52a3bdf7dedc0e3dac73e6bdf72a73d
Fran <yatan87@gmail.com>
2018-07-14 13:56:27 +0200

    Refactor calibration mode #25

--- Arduino_Sensors_Sketch.ino
+++ Arduino_Sensors_Sketch.ino
@@ -125,7 +125,7 @@ const int pins_do[num_DO] = {2};      // DO Ps (Analog)
 const int ldr_pin = 3;                // LDR pin (Analog)
 #endif
       /*   DIGITAL PINS  */
-const int pin_switch_calibracio = 22;
+const int pin_switch_calibracio = 22; // Pin for pH calibration switch
 #define pin_onewire 3                 // where 1-wire is connected
 #define pin_sd_card 4                 // Pin lector SD
 const int pins_rgb[3] = {24,25,26};      // DO RGB Laser Pins (Digital)
@@ -445,6 +445,32 @@ float capture_lux() {
   #elif option_lux == lux_ldr           // Return Lux value with LDR
     return analogRead(ldr_pin);
   #endif
+}
+
+// Capture data in calibration mode
+void do_a_calibration_ph() {
+  if(debug)
+    Serial.println(F("Starting calibration mode..."));
+  char buffer_L[6];              // String buffer
+  
+  lcd.clear();                  // Clear screen
+  lcd.home ();                  // Linea 1
+  lcd.print("pH Calibration");             
+
+  if(num_pH > 0) {
+    lcd.setCursor ( 0, 1 );       // go to the 2nd line
+    lcd.print("pH1:");
+    dtostrf(capture_ph(pins_ph[0]),4,2,buffer_L);
+    lcd.print(buffer_L);
+  }
+  if(num_pH > 1) {
+    lcd.setCursor ( 0, 2 );       // go to the 3rd line
+    lcd.print("pH2:");
+    dtostrf(capture_ph(pins_ph[1]),4,2,buffer_L);
+    lcd.print(buffer_L);
+  }
+  lcd.setCursor ( 3, 3 );       // go to the 4th line
+  lcd.print("OpenSpirulina");
 }
 
 // Mostra per LCD les dades
@@ -879,28 +905,7 @@ void loop() {
 
   // If pin calibration ph switch is HIGH
   while( digitalRead(pin_switch_calibracio) == HIGH ) {
-    if(debug)
-      Serial.println(F("Starting calibration mode..."));
-    char buffer_L[6];              // String buffer
-    
-    lcd.clear();                  // Clear screen
-    lcd.home ();                  // Linea 1
-    lcd.print("pH Calibration");             
-
-    if(num_pH > 0) {
-      lcd.setCursor ( 0, 1 );       // go to the 2nd line
-      lcd.print("pH1:");
-      dtostrf(capture_ph(pins_ph[0]),4,2,buffer_L);
-      lcd.print(buffer_L);
-    }
-    if(num_pH > 1) {
-      lcd.setCursor ( 0, 2 );       // go to the 3rd line
-      lcd.print("pH2:");
-      dtostrf(capture_ph(pins_ph[1]),4,2,buffer_L);
-      lcd.print(buffer_L);
-    }
-    lcd.setCursor ( 3, 3 );       // go to the 4th line
-    lcd.print("OpenSpirulina");
+    do_a_calibration_ph();
     delay(1000);
   }
 
